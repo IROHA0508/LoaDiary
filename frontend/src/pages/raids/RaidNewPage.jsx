de35dd392a8a67d83469879cd744b70f751e3a27
iRoHa0508 <happyday7862@gmail.com>
2026-03-30 04:43:05 +0900

    code fix: RaidNewPage.jsx 에 import 추가

--- frontend/src/pages/raids/RaidNewPage.jsx
+++ frontend/src/pages/raids/RaidNewPage.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom"
 import { useUser } from "../../hooks/useUser";
+import { createRaid } from "../../api/raids";
 
 /* ─────────────────────────────────────────────
    카테고리
