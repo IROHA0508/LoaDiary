7daeeaf8c34f6f95d8fd411b527b8db35e13dadb
iRoHa0508 <happyday7862@gmail.com>
2026-03-30 13:32:44 +0900

    code fix: 백엔드 main.py 프론트엔드 주소 수정

--- backend/main.py
+++ backend/main.py
@@ -27,6 +27,7 @@ app.add_middleware(
 
     # 배포 이후 사용할 프론트엔드 주소
     "https://loakit.vercel.app",
+    "https://loa-diary.vercel.app",
 
     # 백엔드 koyeb 주소
     "https://legitimate-dreddy-lunarproject-10a2d90e.koyeb.app/"
